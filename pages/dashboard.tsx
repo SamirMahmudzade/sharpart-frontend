8cf0f9d6a8760009f12fd5b0f38c05e411c5235e
Archie Smyth <archiesmyth26@googlemail.com>
2021-11-23 18:31:53 +0000

    dashboard WIP - storing state successfully

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import {
       useMoralis,
       useMoralisQuery,
@@ -17,36 +17,92 @@ import NftCard from '../components/Cards/NftCard';
 import NodeCard from "../components/Cards/NodeCard";
 export default function Dashboard() {
 
-      const [uriArray, setUris] = useState([])
+      //? Token state
+      const [nftState, setNftState] = useState({
+            eth: [],
+            polygon: [],
+            avax: [],
+            isDashLoading: false
+      })
 
+      //? Auth user and get address with moralis hook. Address will be passed to useMoralisWeb3ApiCall
       const { isAuthenticated, user, Moralis } = useMoralis()
-      const userAddress: string = user?.get('ethAddress')
+      const userAddress = user?.get('ethAddress')
+
+      //? Assign useMoralisWeb3Api hook to var.
       const web3API = useMoralisWeb3Api()
-      const { fetch, data, error, isLoading } = useMoralisWeb3ApiCall(web3API.account.getNFTs, {
-            chain: 'polygon',
-            format: 'decimal',
+
+      //? hooks for each of the following chains: Ethereum, Polygon, Avalanche
+      const { fetch: fetchEth, data: ethData, error: ethError, isLoading: ethIsLoading } = useMoralisWeb3ApiCall(web3API.account.getNFTs, {
+            chain: 'eth',
+            format: 'decimal' || 'hex',
             address: userAddress
       });
-      //@ts-ignore
-
-
+      const { fetch: fetchPolygon, data: polygonData, error: polygonError, isLoading: polygonIsLoading } = useMoralisWeb3ApiCall(web3API.account.getNFTs, {
+            chain: 'polygon',
+            format: 'decimal' || 'hex',
+            address: userAddress
+      });
+      const { fetch: fetchAvax, data: avaxData, error: avaxError, isLoading: avaxIsLoading } = useMoralisWeb3ApiCall(web3API.account.getNFTs, {
+            chain: 'avalanche',
+            format: 'decimal' || 'hex',
+            address: userAddress
+      });
 
       const getNFTs = async () => {
-            let uriLocalArray = []
-            setUris([])
-            fetch().then(() => {
-                  data?.result.map((item, index) => {
-                        const uri = JSON.parse(item.metadata)
-                        if (uri === null) {
+            //? Clear the state
+            setNftState({
+                  eth: [],
+                  polygon: [],
+                  avax: [],
+                  isDashLoading: true
+            });
+            //? Promise array of different chain fetches for promise.all
+            let promiseArray = [];
+            //? let object for new state after promises
+            let localObj = {
+                  eth: [],
+                  polygon: [],
+                  avax: []
+            }
+            fetchEth().then(() => {
+                  ethData?.result.map((item, index) => {
+                        const nft = JSON.parse(item.metadata)
+
+                        console.log(nft)
+                        if (nft === null) {
                               return
                         }
                         else {
-                              uriLocalArray.push(uri)
+                              localObj.eth.push(nft)
+                              setNftState({ ...nftState, eth: localObj.eth })
                         }
                   })
-                  setUris(uriLocalArray)
             })
+            fetchPolygon().then(() => {
+                  polygonData?.result.map((item, index) => {
+                        const nft = JSON.parse(item.metadata)
+  
+                        console.log(nft)
+                        if (nft === null) {
+                              return
+                        }
+                        else {
+                              localObj.polygon.push(nft)
+                              setNftState({ ...nftState, polygon: localObj.polygon })
+                        }
+                  })
+            })
+
+
       }
+
+
+      useEffect(() => {
+            
+            getNFTs()
+      }, [])
+
       if (!isAuthenticated) return (
             <PageLayout>
                   <AlertCard
@@ -54,7 +110,9 @@ export default function Dashboard() {
                         body='
                   You will need to
                   authenticate to
-                  query with account'
+                  query with account. 
+                  Please select the correct 
+                  account before moving on.'
                         warning
                   />
                   <MoralisAuth />
@@ -68,14 +126,14 @@ export default function Dashboard() {
                   className=''
             >
                   <PageLayout>
+
+
                         <NodeCard>
                               <Heading title={`Welcome ${userAddress}`} fontSize='text-sm sm:text-md md:text-4xl' />
                               <p className='text-center text-th-primary-light'>
-                              View your Polygon 
-                                 Mainnet NFTs
-                               </p>
-                               <Image className='animate-spin ' src={PolygonImg} height={75} width={75} />
-
+                                    View your Ethereum & Polygon NFTs
+                              </p>
+                              <Image className='animate-spin ' src={PolygonImg} height={75} width={75} />
                         </NodeCard>
                         <button
                               onClick={() => getNFTs()}
@@ -93,17 +151,31 @@ export default function Dashboard() {
                               <RefreshIcon width={50} height={50} />
                         </button>
 
+                        <Heading title='Ethereum NFTs' />
                         <div className='flex justify-center items-center'>
                               <div className='grid gap-8
                               grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4
                               items-center justify-center '>
-                                    {uriArray.map((item, index) =>
+                                    {nftState.eth.map((item, index) =>
                                           <div key={index} className='ring-4 flex items-center justify-center'>
                                                 <NftCard nft={item} />
                                           </div>
                                     )}
                               </div>
                         </div>
+                        <Heading title='Polygon NFTs' />
+                        <div className='flex justify-center items-center'>
+                              <div className='grid gap-8
+                              grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4
+                              items-center justify-center '>
+                                    {nftState.polygon.map((item, index) =>
+                                          <div key={index} className='ring-4 flex items-center justify-center'>
+                                                <NftCard nft={item} />
+                                          </div>
+                                    )}
+                              </div>
+                        </div>
+
 
                   </PageLayout>
             </motion.div>
