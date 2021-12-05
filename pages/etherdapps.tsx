2ade2be5c1dd7098c65f10c4d8e8ae67647124a7
Archie Smyth <archiesmyth26@googlemail.com>
2021-12-05 18:03:13 +0000

    etherdapps.tsx - add link for new Dao builder.

--- pages/etherdapps.tsx
+++ pages/etherdapps.tsx
@@ -1,23 +1,26 @@
-
-import AlertCard from '../components/Cards/AlertCard'
-import Link from 'next/link';
+import AlertCard from "../components/Cards/AlertCard";
+import Link from "next/link";
 import { useMoralis } from "react-moralis";
-import MoralisAuth from '../components/Buttons/MoralisAuth';
-
+import MoralisAuth from "../components/Buttons/MoralisAuth";
 
 export default function EtherDapps() {
-      //@ts-ignore
-      const { isAuthenticated, user } = useMoralis();
-      const ethAddress = user?.get('ethAddress')
+  //@ts-ignore
+  const { isAuthenticated, user } = useMoralis();
+  const ethAddress = user?.get("ethAddress");
 
-      //? No Auth
-      if (!isAuthenticated) {
-            return (
-                  <div className="flex flex-col py-16 space-y-4 sm:space-y-10 lg:space-y-16 items-center justify-center ">
-                        <AlertCard title="Whoa There!" body="You require metamask to use these decentralised applications" failure />
-                       <MoralisAuth/>
-                        <Link href="/ethorbtest">
-                              <a className=" 
+  //? No Auth
+  if (!isAuthenticated) {
+    return (
+      <div className="flex flex-col py-16 space-y-4 sm:space-y-10 lg:space-y-16 items-center justify-center ">
+        <AlertCard
+          title="Whoa There!"
+          body="You require metamask to use these decentralised applications"
+          failure
+        />
+        <MoralisAuth />
+        <Link href="/ethorbtest">
+          <a
+            className=" 
                                     subpixel-antialiased  rounded-md
                                     text-center text-xs md:text-base lg:text-3xl
                                     text-th-primary-light
@@ -26,23 +29,55 @@ export default function EtherDapps() {
                                     transition duration-300 ease-in-out 
                                     hover:text-th-primary-medium
                                     transform hover:scale-110 
-                                    ">
-                                    Eth Orb Test - {`(no results are saved, no metamask required)`}
-                              </a>
-
-                        </Link>
-                  
-                  </div>
-            )
-      }
-      //? Authed
-      else return (
-            <div className="flex flex-col py-16 space-y-4 sm:space-y-10 lg:space-y-16 items-center justify-center ">
-
-                  <AlertCard success title="You're all set" body={ethAddress}
-                  />
-                  <Link href="/ethorb">
-                        <a className=" 
+                                    "
+          >
+            Eth Orb Test - {`(no results are saved, no metamask required)`}
+          </a>
+        </Link>
+        <a
+          target="blank"
+          rel="noopener noreferrer"
+          href="https://sharpart-web3-dapp.vercel.app/"
+          className=" 
+                                    subpixel-antialiased  rounded-md
+                                    text-center text-xs md:text-base lg:text-3xl
+                                    text-th-primary-light
+                                    border-b-4 border-th-primary-medium
+                                    hover:border-transparent text-shadow-sm
+                                    transition duration-300 ease-in-out 
+                                    hover:text-th-primary-medium
+                                    transform hover:scale-110 
+                                    "
+        >
+         SharpArt Web3 Dapp - Your dashboard for DEX, Transfers, Balances and more.
+        </a>
+      </div>
+    );
+  }
+  //? Authed
+  else
+    return (
+      <div className="flex flex-col py-16 space-y-4 sm:space-y-10 lg:space-y-16 items-center justify-center ">
+        <AlertCard success title="You're all set" body={ethAddress} />
+        <Link href="/makeadao">
+          <a
+            className=" 
+                                    subpixel-antialiased  rounded-md
+                                    text-center text-xs md:text-base lg:text-3xl
+                                    text-th-primary-light
+                                    border-b-4 border-th-primary-medium
+                                    hover:border-transparent text-shadow-sm
+                                    transition duration-300 ease-in-out 
+                                    hover:text-th-primary-medium
+                                    transform hover:scale-110 
+                                    "
+          >
+            Deploy a DAO contract (in alpha on Mumbai Testnet)
+          </a>
+        </Link>
+        <Link href="/ethorb">
+          <a
+            className=" 
                                 subpixel-antialiased  rounded-md
                                 text-center text-xs md:text-base lg:text-3xl
                                 text-th-primary-light
@@ -51,12 +86,14 @@ export default function EtherDapps() {
                                 transition duration-300 ease-in-out 
                                 hover:text-th-primary-medium
                                 transform hover:scale-110 
-                                    ">
-                              Eth Orb - Guess market movements.
-                        </a>
-                  </Link>
-                  <Link href="/ethorbtest">
-                        <a className="
+                                    "
+          >
+            Eth Orb - Guess market movements.
+          </a>
+        </Link>
+        <Link href="/ethorbtest">
+          <a
+            className="
                              subpixel-antialiased  rounded-md
                              text-center text-xs md:text-base lg:text-3xl
                              text-th-primary-light
@@ -66,13 +103,30 @@ export default function EtherDapps() {
                              hover:text-th-primary-medium
                              transform hover:scale-110 
                                     
-                                    ">
-                              Eth Orb Test - {`(no results are saved, no metamask required)`}
-                        </a>
-                  </Link>
-                  
-            </div>
-      );
+                                    "
+          >
+            Eth Orb Test - {`(no results are saved, no metamask required)`}
+          </a>
+        </Link>
+        <a
+          target="blank"
+          rel="noopener noreferrer"
+          href="https://sharpart-web3-dapp.vercel.app/"
+          className=" 
+                                    subpixel-antialiased  rounded-md
+                                    text-center text-xs md:text-base lg:text-3xl
+                                    text-th-primary-light
+                                    border-b-4 border-th-primary-medium
+                                    hover:border-transparent text-shadow-sm
+                                    transition duration-300 ease-in-out 
+                                    hover:text-th-primary-medium
+                                    transform hover:scale-110 
+                                    "
+        >
+         SharpArt Web3 Dapp - Your dashboard for DEX, Transfers, Balances and more.
+        </a>
+      </div>
+    );
 }
 /*
 export const getServerSideProps: GetServerSideProps = async (context) => {
